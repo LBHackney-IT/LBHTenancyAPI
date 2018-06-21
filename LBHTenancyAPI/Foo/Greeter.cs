70534384d55da4cb4d893416b2cbc2808cdfecf5
Richard Foster <richard@rpf.me>
2018-06-21 12:26:35 +0100

    Test Greeter class using NUnit

---
+++ LBHTenancyAPI/Foo/Greeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LBHTenancyAPI.Foo
+{
+    public class Greeter
+    {
+        public Greeter()
+        {
+        }
+
+        public String SayHello()
+        {
+            return "hello world";
+        }
+    }
+}
---
+++ LBHTenancyAPI/Test/Foo/GreeterTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using LBHTenancyAPI.Foo;
+
+namespace LBHTenancyAPI.Test.Foo
+{
+    [TestFixture]
+    public class GreeterTest
+    {
+        [Test]
+        public void SaysHello()
+        {
+            Greeter greeter = new Greeter();
+            Assert.AreEqual(greeter.SayHello(), "hello world");
+        }
+    }
+}
