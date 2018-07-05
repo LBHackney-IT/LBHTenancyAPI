03d2e3b598d98204bdd3e32401ca2787ad972aa7
Steven Leighton <steven@madetech.com>
2018-07-05 12:31:15 +0100

    Dockerise tests, add host config for CI
    Docker compose version
    
    And override
    
    Change the test execution cmd
    
    DCU
    
    Repo default docker network
    
    set env for build?
    
    Set env on machine

--- LBHTenancyAPITest/Test/StubUniversalHousingFixture.cs
+++ LBHTenancyAPITest/Test/StubUniversalHousingFixture.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Data.SqlClient;
 using Dapper;
+using Microsoft.AspNetCore.Hosting;
 
 namespace LBHTenancyAPITest.Test
 {
     public class DatabaseFixture : IDisposable
     {
         public SqlConnection Db { get; private set; }
+        
         public DatabaseFixture()
         {
             SqlConnectionStringBuilder builder =  new SqlConnectionStringBuilder();  
+       
             builder["Data Source"] = "(local)";
+            
+            if (Environment.GetEnvironmentVariable("CI_TEST") == "True")
+            {
+                builder["Data Source"] = "tcp:stubuniversalhousing";
+            }
+            
             builder["integrated Security"] = false;
             builder["Initial Catalog"] = "StubUH";
             builder.UserID = "sa";
