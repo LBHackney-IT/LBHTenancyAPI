1e8657925116c5c76d67ff62bb66ddd72dd8cc66
Richard Foster <richard@rpf.me>
2018-07-10 11:51:31 +0100

    Randomise tenancy attributes to strengthen ListTenancies tests

--- LBHTenancyAPITest/Test/UseCases/ListTenanciesTest.cs
+++ LBHTenancyAPITest/Test/UseCases/ListTenanciesTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using LBHTenancyAPI.Domain;
 using LBHTenancyAPI.Gateways;
 using LBHTenancyAPI.UseCases;
+using LBHTenancyAPITest.Helpers;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using SQLitePCL;
 using Xunit;
@@ -12,13 +13,6 @@ namespace LBHTenancyAPITest.Test.UseCases
 {
     public class ListTenanciesTest
     {
-        private ITestOutputHelper output;
-
-        public ListTenanciesTest(ITestOutputHelper output)
-        {
-            output = output;
-        }
-
         [Fact]
         public void WhenThereAreNoTenancyRefsGiven_ShouldReturnNone()
         {
@@ -53,74 +47,26 @@ namespace LBHTenancyAPITest.Test.UseCases
         public void WhenATenancyRefIsGiven_ResponseShouldIncludeDetailsOnThatTenancy_Example1()
         {
             var gateway = new StubTenanciesGateway();
-            gateway.SetTenancyListItem("TEST/01", new TenancyListItem()
-            {
-                TenancyRef = "TEST/01",
-                LastActionCode = "ACTION CODE",
-                LastActionDate = new DateTime(2018, 1, 1, 0, 0, 0),
-                CurrentBalance = 1.11,
-                ArrearsAgreementStatus = "CURRENT AGREEMENT STATUS",
-                PrimaryContactName = "RICHARD FOSTER",
-                PrimaryContactShortAddress = "123 TEST STREET",
-                PrimaryContactPostcode = "AB12 C34"
-            });
+            var tenancy = Fake.GenerateTenancyListItem();
 
+            gateway.SetTenancyListItem(tenancy.TenancyRef, tenancy);
 
             var listTenancies = new ListTenancies(tenanciesGateway: gateway);
-            var actualResponse = listTenancies.Execute(tenancyRefs: new List<string> {"TEST/01"});
+            var actualResponse = listTenancies.Execute(tenancyRefs: new List<string> {tenancy.TenancyRef});
             var expectedResponse = new ListTenancies.Response()
             {
                 Tenancies = new List<ListTenancies.ResponseTenancy>
                 {
                     new ListTenancies.ResponseTenancy()
                     {
-                        TenancyRef = "TEST/01",
-                        LastActionCode = "ACTION CODE",
-                        LastActionDate = "2018-01-01 00:00:00Z",
-                        CurrentBalance = "1.11",
-                        ArrearsAgreementStatus = "CURRENT AGREEMENT STATUS",
-                        PrimaryContactName = "RICHARD FOSTER",
-                        PrimaryContactShortAddress = "123 TEST STREET",
-                        PrimaryContactPostcode = "AB12 C34"
-                    }
-                }
-            };
-
-            Assert.Equal(expectedResponse.Tenancies, actualResponse.Tenancies);
-        }
-
-        [Fact]
-        public void WhenATenancyRefIsGiven_ResponseShouldIncludeDetailsOnThatTenancy_Example2()
-        {
-            var gateway = new StubTenanciesGateway();
-            gateway.SetTenancyListItem("TEST/02", new TenancyListItem()
-            {
-                TenancyRef = "TEST/02",
-                LastActionCode = "OTHER ACTION CODE",
-                LastActionDate = new DateTime(2010, 2, 2, 2, 2, 2),
-                CurrentBalance = 10.99,
-                ArrearsAgreementStatus = "COOL AGREEMENT STATUS",
-                PrimaryContactName = "ROBERT FOSTER",
-                PrimaryContactShortAddress = "456 TEST STREET",
-                PrimaryContactPostcode = "CD34 E56"
-            });
-
-            var listTenancies = new ListTenancies(tenanciesGateway: gateway);
-            var actualResponse = listTenancies.Execute(tenancyRefs: new List<string> {"TEST/02"});
-            var expectedResponse = new ListTenancies.Response()
-            {
-                Tenancies = new List<ListTenancies.ResponseTenancy>
-                {
-                    new ListTenancies.ResponseTenancy()
-                    {
-                        TenancyRef = "TEST/02",
-                        LastActionCode = "OTHER ACTION CODE",
-                        LastActionDate = "2010-02-02 02:02:02Z",
-                        CurrentBalance = "10.99",
-                        ArrearsAgreementStatus = "COOL AGREEMENT STATUS",
-                        PrimaryContactName = "ROBERT FOSTER",
-                        PrimaryContactShortAddress = "456 TEST STREET",
-                        PrimaryContactPostcode = "CD34 E56"
+                        TenancyRef = tenancy.TenancyRef,
+                        LastActionCode = tenancy.LastActionCode,
+                        LastActionDate = String.Format("{0:u}", tenancy.LastActionDate),
+                        CurrentBalance = tenancy.CurrentBalance.ToString(),
+                        ArrearsAgreementStatus = tenancy.ArrearsAgreementStatus,
+                        PrimaryContactName = tenancy.PrimaryContactName,
+                        PrimaryContactShortAddress = tenancy.PrimaryContactShortAddress,
+                        PrimaryContactPostcode = tenancy.PrimaryContactPostcode
                     }
                 }
             };
