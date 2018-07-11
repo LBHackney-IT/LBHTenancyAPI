aa4945f335ae654f7dbfc5f92cb18b22274603c8
Steven Leighton <steven@madetech.com>
2018-07-11 15:10:02 +0100

    Switch to decimal (2 places) for balance

--- LBHTenancyAPI/Domain/TenancyListItem.cs
+++ LBHTenancyAPI/Domain/TenancyListItem.cs
@@ -8,17 +8,27 @@ namespace LBHTenancyAPI.Domain
         public string TenancyRef { get; set; }
         public string LastActionCode { get; set; }
         public DateTime LastActionDate { get; set; }
-        public decimal CurrentBalance { get; set; }
+
+        private decimal currentBalance;
+
+        public Decimal CurrentBalance
+        {
+            get => currentBalance;
+
+            set => currentBalance = decimal.Round(value, 2, MidpointRounding.AwayFromZero);
+        }
+
         public string ArrearsAgreementStatus { get; set; }
         public DateTime ArrearsAgreementStartDate { get; set; }
         public string PrimaryContactName { get; set; }
 
         private string primaryContactShortAddress;
+
         public string PrimaryContactShortAddress
         {
             get => primaryContactShortAddress;
 
-            set => primaryContactShortAddress = !string.IsNullOrEmpty(value) ? value.Split("\n").First() : null;
+            set => primaryContactShortAddress = string.IsNullOrWhiteSpace(value) ? null : value.Split("\n").First();
         }
 
         public string PrimaryContactPostcode { get; set; }
