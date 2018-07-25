2437c320d11ca9f5683fc6954aaf4c74ca4c9658
Steven Leighton <steven@madetech.com>
2018-07-25 14:20:02 +0100

    Run code cleanup

--- LBHTenancyAPI/Gateways/UhTenanciesGateway.cs
+++ LBHTenancyAPI/Gateways/UhTenanciesGateway.cs
@@ -61,59 +61,58 @@ namespace LBHTenancyAPI.Gateways
 
         public List<ArrearsActionDiaryEntry> GetActionDiaryEntriesbyTenancyRef(string tenancyRef)
         {
-            return conn.Query<ArrearsActionDiaryEntry>("" +
-                                                         "SELECT " +
-                                                         "tag_ref as TenancyRef, " +
-                                                         "action_code as ActionCode, " +
-                                                         "action_code_name as ActionCodeName, " +
-                                                         "action_date as ActionDate, " +
-                                                         "action_comment as ActionComment, " +
-                                                         "uh_username as UHUsername, " +
-                                                         "action_balance as ActionBalance " +
-                                                         "FROM araction " +
-                                                         $"WHERE tag_ref = ('{tenancyRef}') " +
-                                                         "ORDER BY araction.action_date DESC").ToList();
+            return conn.Query<ArrearsActionDiaryEntry>(
+                "SELECT " +
+                "tag_ref as TenancyRef, " +
+                "action_code as ActionCode, " +
+                "action_code_name as ActionCodeName, " +
+                "action_date as ActionDate, " +
+                "action_comment as ActionComment, " +
+                "uh_username as UHUsername, " +
+                "action_balance as ActionBalance " +
+                "FROM araction " +
+                $"WHERE tag_ref = ('{tenancyRef}') " +
+                "ORDER BY araction.action_date DESC").ToList();
         }
 
         public List<PaymentTransaction> GetPaymentTransactionsByTenancyRef(string tenancyRef)
         {
-            return conn.Query<PaymentTransaction>("" +
-                                                         "SELECT " +
-                                                         "tag_ref AS TenancyRef," +
-                                                         "prop_ref AS PropertyRef, " +
-                                                         "trans_type AS TransactionType, " +
-                                                         "amount AS TransactionAmount, " +
-                                                         "transaction_date AS TransactionDate, " +
-                                                         "trans_ref AS TransactionRef " +
-                                                         "FROM rtrans " +
-                                                         $"WHERE tag_ref = ('{tenancyRef}') " +
-                                                         "ORDER BY transaction_date DESC ").ToList();
+            return conn.Query<PaymentTransaction>(
+                "SELECT " +
+                "tag_ref AS TenancyRef," +
+                "prop_ref AS PropertyRef, " +
+                "trans_type AS TransactionType, " +
+                "amount AS TransactionAmount, " +
+                "transaction_date AS TransactionDate, " +
+                "trans_ref AS TransactionRef " +
+                "FROM rtrans " +
+                $"WHERE tag_ref = ('{tenancyRef}') " +
+                "ORDER BY transaction_date DESC ").ToList();
         }
 
         public Tenancy GetTenancyForRef(string tenancyRef)
         {
-            var result = conn.Query<Tenancy>($"" +
-                                             $"SELECT DISTINCT" +
-                                             $"(tenagree.tag_ref) as TenancyRef, " +
-                                             $"tenagree.cur_bal as CurrentBalance, " +
-                                             $"arag.arag_status as ArrearsAgreementStatus, " +
-                                             $"arag.arag_startdate as ArrearsAgreementStartDate, " +
-                                             $"contacts.con_name as PrimaryContactName, " +
-                                             $"contacts.con_address as PrimaryContactLongAddress, " +
-                                             $"contacts.con_postcode as PrimaryContactPostcode, " +
-                                             $"contacts.con_phone1 as PrimaryContactPhone, " +
-                                             $"araction.action_code as LastActionCode, " +
-                                             $"araction.action_date as LastActionDate " +
-                                             $"FROM tenagree " +
-                                             $"LEFT JOIN arag " +
-                                             $"ON arag.tag_ref = tenagree.tag_ref " +
-                                             $"LEFT JOIN contacts " +
-                                             $"ON contacts.tag_ref = tenagree.tag_ref " +
-                                             $"LEFT JOIN araction " +
-                                             $"ON araction.tag_ref = tenagree.tag_ref " +
-                                             $"WHERE tenagree.tag_ref = ('{tenancyRef}') " +
-                                             $"ORDER BY arag.arag_startdate DESC, araction.action_date DESC")
-                .FirstOrDefault();
+            var result = conn.Query<Tenancy>(
+                "SELECT DISTINCT" +
+                "(tenagree.tag_ref) as TenancyRef, " +
+                "tenagree.cur_bal as CurrentBalance, " +
+                "arag.arag_status as ArrearsAgreementStatus, " +
+                "arag.arag_startdate as ArrearsAgreementStartDate, " +
+                "contacts.con_name as PrimaryContactName, " +
+                "contacts.con_address as PrimaryContactLongAddress, " +
+                "contacts.con_postcode as PrimaryContactPostcode, " +
+                "contacts.con_phone1 as PrimaryContactPhone, " +
+                "araction.action_code as LastActionCode, " +
+                "araction.action_date as LastActionDate " +
+                "FROM tenagree " +
+                "LEFT JOIN arag " +
+                "ON arag.tag_ref = tenagree.tag_ref " +
+                "LEFT JOIN contacts " +
+                "ON contacts.tag_ref = tenagree.tag_ref " +
+                "LEFT JOIN araction " +
+                "ON araction.tag_ref = tenagree.tag_ref " +
+                $"WHERE tenagree.tag_ref = ('{tenancyRef}') " +
+                "ORDER BY arag.arag_startdate DESC, araction.action_date DESC").FirstOrDefault();
 
             result.ArrearsAgreements = GetLastFiveAgreementsForTenancy(tenancyRef);
             result.ArrearsActionDiary = GetLatestFiveArrearsActionForRef(tenancyRef);
@@ -122,34 +121,35 @@ namespace LBHTenancyAPI.Gateways
 
         private List<ArrearsAgreement> GetLastFiveAgreementsForTenancy(string tenancyRef)
         {
-            return conn.Query<ArrearsAgreement>("SELECT TOP 5" +
-                                                      "tag_ref AS TenancyRef," +
-                                                      "arag_status AS Status, " +
-                                                      "arag_startdate Startdate, " +
-                                                      "arag_amount Amount, " +
-                                                      "arag_frequency AS Frequency, " +
-                                                      "arag_breached AS Breached, " +
-                                                      "arag_startbal AS StartBalance, " +
-                                                      "arag_clearby AS ClearBy " +
-                                                      "FROM arag " +
-                                                      $"WHERE tag_ref = '{tenancyRef}'" +
-                                                      "ORDER BY arag_startdate DESC ").ToList();
+            return conn.Query<ArrearsAgreement>(
+                "SELECT TOP 5" +
+                "tag_ref AS TenancyRef," +
+                "arag_status AS Status, " +
+                "arag_startdate Startdate, " +
+                "arag_amount Amount, " +
+                "arag_frequency AS Frequency, " +
+                "arag_breached AS Breached, " +
+                "arag_startbal AS StartBalance, " +
+                "arag_clearby AS ClearBy " +
+                "FROM arag " +
+                $"WHERE tag_ref = '{tenancyRef}'" +
+                "ORDER BY arag_startdate DESC ").ToList();
         }
 
         public List<ArrearsActionDiaryEntry> GetLatestFiveArrearsActionForRef(string tenancyRef)
         {
-            return conn.Query<ArrearsActionDiaryEntry>("" +
-                                                         "SELECT top 5" +
-                                                         "tag_ref as TenancyRef, " +
-                                                         "action_code as ActionCode, " +
-                                                         "action_code_name as ActionCodeName, " +
-                                                         "action_date as ActionDate, " +
-                                                         "action_comment as ActionComment, " +
-                                                         "uh_username as UHUsername, " +
-                                                         "action_balance as ActionBalance " +
-                                                         "FROM araction " +
-                                                         $"WHERE tag_ref = ('{tenancyRef}') " +
-                                                         "ORDER BY araction.action_date DESC").ToList();
+            return conn.Query<ArrearsActionDiaryEntry>(
+                "SELECT top 5" +
+                "tag_ref as TenancyRef, " +
+                "action_code as ActionCode, " +
+                "action_code_name as ActionCodeName, " +
+                "action_date as ActionDate, " +
+                "action_comment as ActionComment, " +
+                "uh_username as UHUsername, " +
+                "action_balance as ActionBalance " +
+                "FROM araction " +
+                $"WHERE tag_ref = ('{tenancyRef}') " +
+                "ORDER BY araction.action_date DESC").ToList();
         }
     }
 }
