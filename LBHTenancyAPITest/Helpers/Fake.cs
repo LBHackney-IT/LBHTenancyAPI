0cb091875b283dd17b346b456ff405b0fba35ab8
Jeff Pinkham <jeff@madetech.com>
2018-08-30 17:42:43 +0100

    reverse fake changes

--- LBHTenancyAPITest/Helpers/Fake.cs
+++ LBHTenancyAPITest/Helpers/Fake.cs
@@ -11,21 +11,23 @@ namespace LBHTenancyAPITest.Helpers
     {
         public static TenancyListItem GenerateTenancyListItem()
         {
-            var random = new Faker();
+          var random = new Faker();
 
-            return new TenancyListItem
-            {
-                TenancyRef = random.Random.Hash(11),
-                CurrentBalance = random.Finance.Amount(),
-                LastActionDate = new DateTime(random.Random.Int(1900, 1999), random.Random.Int(1, 12), random.Random.Int(1, 28), 9, 30, 0),
-                LastActionCode = random.Random.Hash(3),
-                ArrearsAgreementStatus = random.Random.Hash(10),
-                ArrearsAgreementStartDate =
+          return new TenancyListItem
+          {
+              TenancyRef = random.Random.Hash(11),
+              PropertyRef = random.Random.Hash(12),
+              Tenure = random.Random.Hash(3),
+              CurrentBalance = random.Finance.Amount(),
+              LastActionDate = new DateTime(random.Random.Int(1900, 1999), random.Random.Int(1, 12), random.Random.Int(1, 28), 9, 30, 0),
+              LastActionCode = random.Random.Hash(3),
+              ArrearsAgreementStatus = random.Random.Hash(10),
+              ArrearsAgreementStartDate =
                   new DateTime(random.Random.Int(1900, 1999), random.Random.Int(1, 12), random.Random.Int(1, 28), 9, 30, 0),
-                PrimaryContactName = random.Name.FullName(),
-                PrimaryContactShortAddress = $"{random.Address.BuildingNumber()}\n{random.Address.StreetName()}\n{random.Address.Country()}",
-                PrimaryContactPostcode = random.Random.Hash(10)
-            };
+              PrimaryContactName = random.Name.FullName(),
+              PrimaryContactShortAddress = $"{random.Address.BuildingNumber()}\n{random.Address.StreetName()}\n{random.Address.Country()}",
+              PrimaryContactPostcode = random.Random.Hash(10)
+          };
         }
 
         public static PaymentTransaction GeneratePaymentTransactionDetails()
@@ -50,7 +52,12 @@ namespace LBHTenancyAPITest.Helpers
             return new Tenancy
             {
                 TenancyRef = random.Random.Hash(11),
+                PropertyRef = random.Random.Hash(12),
+                Tenure = random.Random.Hash(3),
                 CurrentBalance = random.Finance.Amount(),
+                Rent = random.Finance.Amount(),
+                Service = random.Finance.Amount(),
+                OtherCharge = random.Finance.Amount(),
                 PrimaryContactName = random.Name.FullName(),
                 PrimaryContactLongAddress = $"{random.Address.BuildingNumber()}\n{random.Address.StreetName()}\n{random.Address.Country()}",
                 PrimaryContactPostcode = random.Random.Hash(10),
