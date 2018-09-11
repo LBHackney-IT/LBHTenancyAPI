f308cb5948040916de724e8fac911adf0852e53c
Rashmi <rashmi.shetty@hackney.gov.uk>
2018-09-11 15:55:16 +0100

    Added fluent validation for Arrears Agreements

--- LBHTenancyAPI/UseCases/ArrearsAgreements/CreateArrearsAgreementRequest.cs
+++ LBHTenancyAPI/UseCases/ArrearsAgreements/CreateArrearsAgreementRequest.cs
@@ -21,7 +21,23 @@ namespace LBHTenancyAPI.UseCases.ArrearsAgreements
             {
                 RuleFor(x => x).NotNull();
                 RuleFor(x => x.AgreementInfo).NotNull().WithMessage("AgreementInfo cannot be null");
+                RuleFor(x => x.AgreementInfo.ArrearsAgreementStatusCode).NotNull().WithMessage("Arrears Agreement Status Code cannot be null");
+                RuleFor(x => x.AgreementInfo.StartBalance).NotNull().WithMessage("StartBalance cannot be null");
+                RuleFor(x => x.AgreementInfo.StartDate).NotNull().WithMessage("Start Date cannot be null");
+                RuleFor(x => x.AgreementInfo.Comment).NotEmpty().WithMessage("Comment cannot be empty");
+                RuleFor(x => x.AgreementInfo.TenancyAgreementRef).NotEmpty().WithMessage("TenancyAgreementRef cannot be empty");
+                RuleFor(x => x.AgreementInfo.IsBreached).NotEmpty().WithMessage("IsBreached cannot be empty");
+                RuleFor(x => x.AgreementInfo.FirstCheck).NotEmpty().WithMessage("FirstCheck cannot be empty");
+                RuleFor(x => x.AgreementInfo.FirstCheckFrequencyTypeCode).NotNull().WithMessage("FirstCheckFrequencyTypeCode cannot be null");
+                RuleFor(x => x.AgreementInfo.NextCheck).NotNull().WithMessage("NextCheck cannot be null");
+                RuleFor(x => x.AgreementInfo.FcaDate).NotNull().WithMessage("FCADate cannot be null");
+                RuleFor(x => x.AgreementInfo.FcaDate).NotEmpty().WithMessage("FCADate is required")
+                                       .GreaterThan(x => x.AgreementInfo.StartDate).WithMessage("FCA date must be after Start date")
+                                       .When(x => x.AgreementInfo.StartDate.HasValue);
+                RuleFor(x => x.AgreementInfo.MonitorBalanceCode).NotNull().WithMessage("MonitorBalanceCode cannot be null");
+
                 RuleFor(x => x.PaymentSchedule).NotEmpty().WithMessage("Please specify a payment schedule");
+                RuleFor(x => x.PaymentSchedule).NotNull().WithMessage("FCADate cannot be null");
             }
         }
     }
