333111322ed397f25202dd77ed46508db1bde87d
Jeff Pinkham <jeff@madetech.com>
2018-09-12 17:35:57 +0100

    Remove unused class and add missing message,

--- LBHTenancyAPI/Extensions/Validation/CreateArrearsActionDiaryExtensions.cs
+++ LBHTenancyAPI/Extensions/Validation/CreateArrearsActionDiaryExtensions.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AgreementService;
 using FluentValidation;
-using LBHTenancyAPI.Infrastructure.API;
 
 namespace LBHTenancyAPI.Extensions.Validation
 {
@@ -27,9 +22,8 @@ namespace LBHTenancyAPI.Extensions.Validation
             RuleFor(x => x).NotNull();
             RuleFor(x => x.ArrearsAction).NotNull().WithMessage("Arrears Action cannot be null");
             RuleFor(x => x.ArrearsAction.TenancyAgreementRef).NotEmpty().WithMessage("Please specify a tenancy reference");
-            //RuleFor(x => x.ArrearsAction.ActionBalance).NotNull().WithMessage("Please specify an action balance");
             RuleFor(x => x.ArrearsAction.ActionCode).NotEmpty().NotNull().WithMessage("Please specify an action code");
-            RuleFor(x => x.ArrearsAction.Comment).NotEmpty().NotNull();
+            RuleFor(x => x.ArrearsAction.Comment).NotEmpty().NotNull().WithMessage("Please specify a comment");
         }
     }
 }
