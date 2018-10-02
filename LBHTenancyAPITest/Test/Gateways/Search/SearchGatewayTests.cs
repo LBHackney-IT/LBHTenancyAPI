22f94b4902908bfdfc12510cd455e8548417c60a
Jeff Pinkham <jeff@madetech.com>
2018-10-02 15:34:41 +0100

    outline of tests

--- LBHTenancyAPITest/Test/Gateways/Search/SearchGatewayTests.cs
+++ LBHTenancyAPITest/Test/Gateways/Search/SearchGatewayTests.cs
@@ -7,12 +7,79 @@ using System.Threading;
 using System.Threading.Tasks;
 using Bogus;
 using LBH.Data.Domain;
+using LBHTenancyAPI.Gateways.Search;
 using LBHTenancyAPI.UseCases.Contacts.Models;
+using Xunit;
 
 namespace LBHTenancyAPITest.Test.Gateways.Search
 {
     public class SearchGatewayTests
     {
+        private ISearchGateway _classUnderTest;
 
+        [Theory]
+        [InlineData("Smith")]
+        [InlineData("Shetty")]
+        public async Task can_search_on_last_name(string lastName)
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+
+        [Theory]
+        [InlineData("Jeff")]
+        [InlineData("Rashmi")]
+        public async Task can_search_on_first_name(string firstName)
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+
+        [Theory]
+        [InlineData("E8 1HH")]
+        [InlineData("E8 1EA")]
+        public async Task can_search_on_post_code(string postCode)
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+
+        [Theory]
+        [InlineData("17 Reading Lane")]
+        [InlineData("Hackney Contact Center")]
+        public async Task can_search_on_short_address(string shortAddress)
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
+
+        [Theory]
+        [InlineData("000017/01")]
+        [InlineData("000018/01")]
+        public async Task can_search_on_tenancy_reference(string shortAddress)
+        {
+            //arrange
+
+            //act
+
+            //assert
+
+        }
     }
 }
