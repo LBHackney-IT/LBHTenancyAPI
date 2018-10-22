d27af366630a379bff5134b98e050e3009b80c58
Jeff Pinkham <jeff@madetech.com>
2018-10-22 14:47:57 +0100

    clean up

--- LBHTenancyAPI/Controllers/SearchController.cs
+++ LBHTenancyAPI/Controllers/SearchController.cs
@@ -1,9 +1,6 @@
-using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using LBHTenancyAPI.Extensions.Controller;
 using LBHTenancyAPI.Infrastructure.API;
-using LBHTenancyAPI.UseCases.Contacts;
-using LBHTenancyAPI.UseCases.Contacts.Models;
 using LBHTenancyAPI.UseCases.Search;
 using LBHTenancyAPI.UseCases.Search.Models;
 using Microsoft.AspNetCore.Mvc;
