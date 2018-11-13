f794815e49272d1a3c56f2ddc67569b3d032bce3
LBHSPreston <selwyn.preston@hackney.gov.uk>
2018-11-13 13:21:24 +0000

    Added comments for swagger documentation.

--- LBHTenancyAPI/Controllers/V2/ArrearsActionDiaryController.cs
+++ LBHTenancyAPI/Controllers/V2/ArrearsActionDiaryController.cs
@@ -8,6 +8,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LBHTenancyAPI.Controllers.V2
 {
+    /// <summary>
+    /// Arrears Action Diary Controller V2 to create an action diary record with the specified user recorded
+    /// </summary>
     [ApiVersion("2")]
     [Produces("application/json")]
     [Route("api/v{apiVersion:apiVersion}/tenancies/arrears-action-diary/", Name = "ArrearsActionDiaryV2")]
@@ -20,7 +23,19 @@ namespace LBHTenancyAPI.Controllers.V2
             _createArrearsActionDiaryUseCase = createArrearsActionDiaryUseCase;
         }
 
-        [HttpPost]
+        /// <summary>
+        /// Arrears Action Diary Controller V2 to create an action diary record with the specified user recorded
+        /// </summary>
+        /// <param name="actionBalance"></param>
+        /// <param name="actionCategory"></param>
+        /// <param name="actionCode"></param>
+        /// <param name="comment"></param>
+        /// <param name="tenancyAgreementRef"></param>
+        /// <param name="companyCode"></param>
+        /// <param name="appUser"></param>
+        /// <param name="sessionToken"></param>
+        /// <returns></returns>
+        [HttpPost, MapToApiVersion("2")]
         public async Task<IActionResult> Post([FromBody][Required] ActionDiaryRequest request)
         {
 
